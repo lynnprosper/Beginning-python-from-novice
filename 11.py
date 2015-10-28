e81d809a9290ca1c8b214eff916810e6d3d93d6a
Miles <lynnprosper@gmail.com>
2015-10-29 01:36:32 +0800

    22-04

--- 11.py
+++ 11.py
@@ -1,30 +1,76 @@
 from xml.sax.handler import ContentHandler
 from xml.sax import parse
+import os
 
-class PageMaker(ContentHandler):
-	passthrough = False
+class Dispatcher:
+	def  dispatch(self, prefix, name, attrs=None):
+		mname = prefix + name.capitalize()
+		dname = 'default' + prefix.capitalize()
+		method = getattr(self, mname, None)
+		if callable(method): args = ()
+		else:
+			method = getattr(self, dname, None)
+			args = name,
+		if prefix == 'start': args += attrs,
+		if callable(method): method(*args)
+		
 	def startElement(self, name, attrs):
-		if name == 'page':
-			self.passthrough = True
-			self.out = open(attrs['name'] + '.html', 'w')
-			self.out.write('<html><head>\n')
-			self.out.write('<title>%s</title>\n' % attrs['title'])
-			self.out.write('</head><body>\n')
-		elif self.passthrough:
+		self.dispatch('start', name, attrs)
+		
+	def endElement(self, name):
+		self.dispatch('end', name)
+		
+class WebsiteConstructor(Dispatcher, ContentHandler):
+
+	passthrough = False
+
+	def __init__(self, directory):
+		self.directory = [directory]
+		self.ensureDirectory()
+		
+	def ensureDirectory(self):
+		path = os.path.join(*self.directory)
+		if not os.path.isdir(path): os.makedirs(path)
+		
+	def characters(self, chars):
+		if self.passthrough: self.out.write(chars)
+		
+	def defaultStart(self, name, attrs):
+	
+		if self.passthrough:
 			self.out.write('<' + name)
 			for key, val in attrs.items():
 				self.out.write(' %s="%s"' % (key, val))
 			self.out.write('>')
 			
-	def endElement(self, name):
-		if name == 'page':
-			self.passthrough = False
-			self.out.write('\n</body></html>\n')
-			self.out.close()
-		elif self.passthrough:
+	def defaultEnd(self, name):
+		if self.passthrough:
 			self.out.write('</%s>' % name)
 			
-	def characters(self, chars):
-		if self.passthrough: self.out.write(chars)
+	def startDirectory(self, attrs):
+		self.directory.append(attrs['name'])
+		self.ensureDirectory()
 		
-parse('website.xml', PageMaker())
+	def endDirectory(self):
+		self.directory.pop()
+		
+	def startPage(self, attrs):
+		filename = os.path.join(*self.directory + [attrs['name']+'.html'])
+		self.out = open(filename, 'w')
+		self.writeHeader(attrs['title'])
+		self.passthrough = True
+		
+	def endPage(self):
+		self.passthrough = False
+		self.writeFooter()
+		self.out.close()
+		
+	def writeHeader(self, title):
+		self.out.write('<html>\n <head>\n	<title>')
+		self.out.write(title)
+		self.out.write('</title>\n </head>\n	<body>\n')
+		
+	def writeFooter(self):
+		self.out.write('\n <\body>\n</html>\n')
+		
+parse('website.xml', WebsiteConstructor('public_html'))
