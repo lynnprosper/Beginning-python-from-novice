72e4684112b79c4c04e066217d0b0c1d19c341ee
Miles <lynnprosper@gmail.com>
2015-11-06 23:35:10 +0800

    27-01

--- 11.py
+++ 11.py
@@ -1,23 +1,76 @@
-#!/usr/bin/env python
+from xmlrpclib import ServerProxy
+from os.path import join, isfile
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from urlparse import urlparse
+import sys
 
-import cgi
-form = cgi.FieldStorage()
+MAX_HISTORY_LENGTH = 6
 
-text = form.getvalue('text', open('simple_edit.dat').read())
-f = open('simple_edit.dat', 'w')
-f.write(text)
-f.close()
-print """Content-type: text/html
+OK = 1
+FAIL = 2
+EMPTY = ''
 
-<html>
-  <head>
-    <title>A Simple Editor</title>
-  </head>
-  <body>
-    <form action = 'simple_edit.cgi' method = 'POST'>
-	<textarea rows = '10' cols = '20' name = 'text'>%s</textarea><br />
-	<input type = 'submit' />
-	</form>
-  </body>
- </html>
- """ % text
+def getPort(url):
+	name = urlparse(url)[1]
+	parts = name.split(':')
+	return int(parts[-1])
+	
+class Node:
+	def __init__(self, url, dirname, secret):
+		self.url = url
+		self.dirname = dirname
+		self.secret = secret
+		self.known = set()
+	def query(self, query, history=[]):
+		code, data = self._handle(query)
+		if code == OK:
+			return code, data
+		else:
+			history = history + [self.url]
+			if len(history) >= MAX_HISTORY_LENGTH:
+				return FAIL, EMPTY
+			return self._broadcast(query, history)
+			
+	def hello(self, other):
+		self.known.add(other)
+		return OK
+	def fetch(self, query, secret):
+		if secret != self.secret: return FAIL
+		code, data = self.query(query)
+		if code == OK:
+			f = open(join(self.dirname, query), 'w')
+			f.write(data)
+			f.close()
+			return OK
+		else:
+			return FAIL
+		
+	def _start(self):
+		s = SimpleXMLRPCServer(("", getPort(self.url)), logRequests = False)
+		s.register_instance(self)
+		s.serve_forever()
+		
+	def _handle(self, query):
+		dir = self.dirname
+		name = join(dir, query)
+		if not isfile(name): return FAIL, EMPTY
+		return OK, open(name).read()
+		
+	def _broadcast(self, query, history):
+		for other in self.known.copy():
+			if other in history: continue
+			try:
+				s = ServerProxy(other)
+				code, data = s.query(query, history)
+				if code == OK:
+					return code, data
+			except:
+				self.known.remove(other)
+		return FAIL, EMPTY
+		
+def main():
+		url, directory, secret = sys.argv[1:]
+		n = Node(url, directory, secret)
+		n._start()
+		
+if __name__ == '__main__': main()
